98a52071025450057562e1b2ca7eaf837e5bb352
Sin0n0me <zakokamikami@icloud.com>
2026-09-19 20:47:41 +0900

    GLSL sampler bindingとtexture unitの対応を保持する TextureUnitBinder を追加

---
+++ src/engine/renderer/src/renderer/opengl/common/texture_unit_binder.cpp
@@ -0,0 +1,11 @@
+#include "texture_unit_binder.h"
+
+namespace enishi::renderer::opengl {
+    void TextureUnitBinder::bind(const std::uint32_t binding, const std::uint32_t texture) noexcept {
+        this->textures.insert_or_assign(binding, texture);
+    }
+    std::uint32_t TextureUnitBinder::get(const std::uint32_t binding) const noexcept {
+        const auto iter = this->textures.find(binding);
+        return iter == this->textures.end() ? 0 : iter->second;
+    }
+} // namespace enishi::renderer::opengl
