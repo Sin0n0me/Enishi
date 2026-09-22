50fc027dab766cbb631f11683b1733cc04f019e6
Sin0n0me <zakokamikami@icloud.com>
2026-09-22 20:48:28 +0900

    PMX 対応部の規約違反を修正

--- src/engine/assets_system/src/assets_system/model/model_loader/pmx/pmx_reader.h
+++ src/engine/assets_system/src/assets_system/model/model_loader/pmx/pmx_reader.h
@@ -2,6 +2,8 @@
 #include "pmx_model_loader.h"
 
 namespace enishi::assets_system {
+    class PMXReader;
+
     [[nodiscard]] foundation::Result<PMXData, AssetError> parse_pmx(
         std::span<const std::uint8_t> bytes);
 } // namespace enishi::assets_system
